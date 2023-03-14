862ce0623ef15e5c48c74b6b2641007b1784c155
Antonio Marchese <01645194400@estudante.ufjf.br>
2023-03-14 16:08:19 -0300

    alterando host da aplicação

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
-app.listen(3333, () => {
+app.listen(3333, "0.0.0.0", () => {
   console.log("Server running at port 3333");
 });
