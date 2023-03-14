fa126e8f8f1bd50cd2f16defbaa3b0bfd5fe2da1
Antonio Marchese <01645194400@estudante.ufjf.br>
2023-03-14 15:53:46 -0300

    alterando o banco de dados e resolvendo a query de monthSummary

--- src/services/habit.service.ts
+++ src/services/habit.service.ts
@@ -277,7 +277,7 @@ export const habitService = {
         JOIN habits H
           ON H.id = HWD.habit_id
         WHERE 
-          HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+          HWD.week_day = CAST (EXTRACT (DOW FROM D.date) AS INTEGER)
           AND H.created_at <= D.date
           AND H.user_id = ${user_id}
       ) as amount
